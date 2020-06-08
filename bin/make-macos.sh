780c7131ea3dc003ad6cf5e2f3fbaa74af71559d
A.J <andrwj@gmail.com>
2020-06-08 19:22:34 +0900

    TPM 설정을 분리

--- bin/make-macos.sh
+++ bin/make-macos.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos geovim.conf > ~/.tmux.conf.local
+# cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos geovim.conf > ~/.tmux.conf.local
+cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos > ~/.tmux.conf.local
