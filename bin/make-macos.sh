434435d560c62251ddb5e4abd91106e753a4ee3b
A.J <andrwj@nntuple.com>
2021-09-12 03:53:52 +0900

    tpm 관련 설정파일 제거

--- bin/make-macos.sh
+++ bin/make-macos.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos geovim.conf > ~/.tmux.conf.local
-cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos tmux-prefix.conf tmux-lab.conf tmux-plugins.conf > ~/.tmux.conf.local
+# cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos tmux-prefix.conf tmux-lab.conf tmux-plugins.conf > ~/.tmux.conf.local
+cd ~/.tmux && cat .tmux.conf.local tmux.conf.common tmux.conf.macos tmux-prefix.conf tmux-lab.conf > ~/.tmux.conf.local
